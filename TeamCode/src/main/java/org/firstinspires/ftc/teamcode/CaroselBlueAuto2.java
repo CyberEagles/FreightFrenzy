10aea8ead2958b40fb3a658a2b3722f039ddf55b
AzureAD\JacobZarowny <j.zarowny19@gmail.com>
2021-11-25 19:03:59 -0700

    Added the auto work form last day

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CaroselBlueAuto2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CaroselBlueAuto2.java
@@ -10,7 +10,7 @@ import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
 import java.util.List;
 @Autonomous
-public class CaroselBlueAuto extends LinearOpMode {
+public class CaroselBlueAuto2 extends LinearOpMode {
     OdometerHardware robot = new OdometerHardware(this);
     @Override
 
@@ -19,7 +19,12 @@ public class CaroselBlueAuto extends LinearOpMode {
 
 
         waitForStart();
-        
+        robot.DuckyDropper.setPower(0.6);
+        robot.goToPosition(-9,0,0.3,0,0.5,5, robot.STRAFELEFT);
+        robot.goToPosition(-9,20,0.3,0,0.5,5, robot.FORWARD);
+//        robot.turn(0.5,10,2,5);
+        sleep(2000);
+        robot.DuckyDropper.setPower(0);
 
     }
 }
