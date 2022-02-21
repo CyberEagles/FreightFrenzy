901a1f99476725e2cc5b91f740dd483c1fbcea89
AzureAD\JacobZarowny <j.zarowny19@gmail.com>
2022-02-21 15:11:27 -0700

    Added an auto for the red side that drops off the block WIP

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedBlockOnly.java
@@ -0,0 +1,88 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+@Autonomous
+public class RedBlockOnly extends LinearOpMode {
+    OpenCvCamera camera;
+    OdometerHardware robot = new OdometerHardware(this);
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.initDriveHardwareMap();
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        int cameraMonitorViewId = hardwareMap.appContext
+                .getResources().getIdentifier("cameraMonitorViewId",
+                        "id", hardwareMap.appContext.getPackageName());
+        OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        opencvRed detector = new opencvRed(telemetry);
+        camera.setPipeline(detector);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(1920,1080, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+
+        waitForStart();
+        switch (detector.getLocation()) {
+            case LEFT:
+                telemetry.addData("Left side","proceed");
+                telemetry.update();
+                robot.Slides.setTargetPosition(-500);
+                robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.Slides.setPower(-1.0);
+                robot.goToPosition(-48,0,0.3,0,1,5,robot.STRAFELEFT);
+                robot.turn(0.2,0,2,5);
+                robot.drivedistance(1,0.2,5,robot.BACKWARD);
+                robot.cargo.setPosition(0.5);
+                sleep(1000);
+                robot.cargo.setPosition(1);
+                break;
+            case RIGHT:
+                telemetry.addData("Right Side","proceed");
+                telemetry.update();
+                robot.Slides.setTargetPosition(-2000);
+                robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.Slides.setPower(-1.0);
+                robot.goToPosition(-48,0,0.3,0,1,5,robot.STRAFELEFT);
+                robot.turn(0.2,0,2,5);
+                robot.drivedistance(1,0.2,5,robot.BACKWARD);
+                robot.cargo.setPosition(0.5);
+                sleep(1000);
+                robot.cargo.setPosition(1);
+                break;
+
+            case MIDDLE:
+                telemetry.addData("Middle", "proceed");
+                telemetry.update();
+                robot.Slides.setTargetPosition(-1200);
+                robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                robot.Slides.setPower(-1.0);
+                robot.goToPosition(-48,0,0.3,0,1,5,robot.STRAFELEFT);
+                robot.turn(0.2,0,2,5);
+                robot.drivedistance(1,0.2,5,robot.BACKWARD);
+                robot.cargo.setPosition(0.5);
+                sleep(1000);
+                robot.cargo.setPosition(1);
+                break;
+
+            case NOT_FOUND:
+                telemetry.addData("not found","proceed");
+                telemetry.update();
+        }
+        camera.stopStreaming();
+    }
+}
