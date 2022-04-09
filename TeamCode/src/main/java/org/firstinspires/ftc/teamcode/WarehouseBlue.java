9f06a81f7c3033ff80c7c17de5805d2cb8b1f803
Ben Loewen <bloewen999@gmail.com>
2022-04-09 16:03:16 -0600

    CarouselBlue just needs tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WarehouseBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WarehouseBlue.java
@@ -25,7 +25,7 @@ public class WarehouseBlue extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive robot = new SampleMecanumDrive(hardwareMap);
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 2");
         int cameraMonitorViewId = hardwareMap.appContext
                 .getResources().getIdentifier("cameraMonitorViewId",
                         "id", hardwareMap.appContext.getPackageName());
@@ -60,7 +60,43 @@ public class WarehouseBlue extends LinearOpMode {
                         SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL*0.8))
                 .build();
 
-        TrajectorySequence Intake1 = robot.trajectorySequenceBuilder(Right.end())
+        TrajectorySequence Middle = robot.trajectorySequenceBuilder(startPose)
+                .addDisplacementMarker(()-> {
+                    robot.Slides.setTargetPosition(-1200);
+                    robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.Slides.setPower(-1);
+                })
+                .splineToLinearHeading(new Pose2d(-3, 38.5, Math.toRadians(70)), Math.toRadians(0),
+                        SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL*0.8, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL*0.8))
+                .build();
+
+        TrajectorySequence Left = robot.trajectorySequenceBuilder(startPose)
+                .addDisplacementMarker(()-> {
+                    robot.Slides.setTargetPosition(-600);
+                    robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.Slides.setPower(-1);
+                })
+                .splineToLinearHeading(new Pose2d(-3.5, 37.5, Math.toRadians(70)), Math.toRadians(0),
+                        SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL*0.6, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL*0.6))
+                .build();
+
+        waitForStart();
+        if(isStopRequested()) return;
+        Pose2d intakeStartPose = Left.end();
+        switch (detector.getLocation()) {
+            case LEFT:
+                intakeStartPose = Left.end();
+                break;
+            case MIDDLE:
+                intakeStartPose = Middle.end();
+                break;
+            case RIGHT:
+                intakeStartPose = Right.end();
+                break;
+        }
+        TrajectorySequence Intake1 = robot.trajectorySequenceBuilder(intakeStartPose)
                 .addDisplacementMarker(()->{
                     robot.cargo.setPosition(1);
                     robot.Slides.setTargetPosition(0);
@@ -72,7 +108,20 @@ public class WarehouseBlue extends LinearOpMode {
                 .splineTo(new Vector2d(47, 61.5), Math.toRadians(0))
                 .build();
 
-        TrajectorySequence Intake2 = robot.trajectorySequenceBuilder(Right.end())
+        TrajectorySequence Blocks = robot.trajectorySequenceBuilder(Intake1.end())
+                .splineToConstantHeading(new Vector2d(9, 61.5), Math.toRadians(0), SampleMecanumDrive.getVelocityConstraint
+                                (DriveConstants.MAX_VEL*0.8, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
+                        SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL*0.8))
+                .addDisplacementMarker(()-> {
+                    robot.Intake.setPower(0);
+                    robot.Slides.setTargetPosition(-2000);
+                    robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    robot.Slides.setPower(-1);
+                })
+                .splineToLinearHeading(new Pose2d(-2.5, 39, Math.toRadians(70)), Math.toRadians(0))
+                .build();
+
+        TrajectorySequence Intake2 = robot.trajectorySequenceBuilder(Blocks.end())
                 .waitSeconds(1)
                 .addDisplacementMarker(()->{
                     robot.cargo.setPosition(1);
@@ -82,135 +131,188 @@ public class WarehouseBlue extends LinearOpMode {
                     robot.Intake.setPower(0.9);
                 })
                 .splineToLinearHeading(new Pose2d(9, 62, Math.toRadians(0)), Math.toRadians(0))
-                .splineTo(new Vector2d(55, 61.5), Math.toRadians(0))
-                .build();
-
-        TrajectorySequence Blocks = robot.trajectorySequenceBuilder(Intake1.end())
-                .splineToConstantHeading(new Vector2d(9, 61.5), Math.toRadians(0))
-                .addDisplacementMarker(()-> {
-                    robot.Intake.setPower(0);
-                    robot.Slides.setTargetPosition(-2000);
-                    robot.Slides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    robot.Slides.setPower(-1);
-                })
-                .splineToLinearHeading(new Pose2d(-2.5, 39, Math.toRadians(70)), Math.toRadians(0),
+                .splineTo(new Vector2d(52, 61.5), Math.toRadians(0),
                         SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL*0.8, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH),
                         SampleMecanumDrive.getAccelerationConstraint(DriveConstants.MAX_ACCEL*0.8))
                 .build();
 
 
-        TrajectorySequence Middle = robot.trajectorySequenceBuilder(startPose)
-                .addDisplacementMarker(()->{
-                    robot.Slides.setTargetPosition(-1200);
-                    robot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    robot.Slides.setPower(-1);
-                })
-                .lineToLinearHeading(new Pose2d(-8, 43, Math.toRadians(80)))
-                .addDisplacementMarker(()->{
-                    robot.Slides.setTargetPosition(0);
-                    robot.Slides.setPower(1);
-                })
-                .lineToLinearHeading(new Pose2d(0, 61.5, Math.toRadians(0)))
-                .forward(40)
-                .strafeRight(20)
-                .build();
-
-        TrajectorySequence Left = robot.trajectorySequenceBuilder(startPose)
-                .addDisplacementMarker(()->{
-                    robot.Slides.setTargetPosition(-500);
-                    robot.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                    robot.Slides.setPower(-1);
-                })
-                .lineToLinearHeading(new Pose2d(-8, 43, Math.toRadians(80)))
-                .addDisplacementMarker(()->{
-                    robot.Slides.setTargetPosition(0);
-                    robot.Slides.setPower(1);
-                })
-                .lineToLinearHeading(new Pose2d(0, 61, Math.toRadians(0)))
-                .forward(40)
-                .strafeRight(20)
-                .build();
-
-        waitForStart();
-        if(isStopRequested()) return;
-
-
-//        switch (detector.getLocation()) {
-  //          case LEFT:
+        switch (detector.getLocation()) {
+            case LEFT:
                 telemetry.addData("Left side","proceed");
                 telemetry.update();
-                robot.capper.setPosition(0.2);
+                robot.capper.setPosition(0);
+                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.HOT_PINK);
+                robot.followTrajectorySequence(Left);
+                robot.cargo.setPosition(0);
+                sleep(1000);
+                while (opModeIsActive() && !isStopRequested()) {
+                    telemetry.addData("Distance", robot.distance.getDistance(DistanceUnit.CM));
+                    telemetry.update();
+                    robot.followTrajectorySequenceAsync(Intake1);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    robot.followTrajectorySequenceAsync(Intake2);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    break;
+                }
+                robot.followTrajectorySequence(Intake2);
+
+                break;
+
+
+            case RIGHT:
+                telemetry.addData("Right Side","proceed");
+                telemetry.update();
+                robot.capper.setPosition(0);
                 robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.LIME);
                 robot.followTrajectorySequence(Right);
                 robot.cargo.setPosition(0);
                 sleep(1000);
-
-//                robot.followTrajectorySequence(Blocks);
-//                robot.cargo.setPosition(0);
-//                robot.followTrajectorySequenceAsync(Intake2);
-//                robot.followTrajectorySequence(Blocks);
-//                robot.cargo.setPosition(0);
-
-
-
-
-
-    //            break;
-
-//            case RIGHT:
-//                telemetry.addData("Right side","proceed");
-//                telemetry.update();
-//                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.HOT_PINK);
-//                robot.followTrajectorySequence(Right);
-//                break;
-//
-//            case MIDDLE:
-//                telemetry.addData("Middle Position","proceed");
-//                telemetry.update();
-//                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
-//                robot.followTrajectorySequence(Middle);
-//                break;
-//
-//            case NOT_FOUND:
-//                telemetry.addData("Not Found","proceed");
-//                telemetry.update();
-//                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.LIME);
-//                robot.followTrajectorySequence(Left);
-                 //   break;
-
-
-        while (opModeIsActive() && !isStopRequested()) {
-            telemetry.addData("Distance", robot.distance.getDistance(DistanceUnit.CM));
-            telemetry.update();
-            robot.followTrajectorySequenceAsync(Intake1);
-            while (opModeIsActive() && !isStopRequested()){
-                if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
-                    robot.breakFollowing();
-                    robot.Intake.setPower(-1);
-                    robot.followTrajectorySequence(Blocks);
-                    robot.cargo.setPosition(0);
-                    sleep(1000);
-                    robot.cargo.setPosition(1);
+                while (opModeIsActive() && !isStopRequested()) {
+                    telemetry.addData("Distance", robot.distance.getDistance(DistanceUnit.CM));
+                    telemetry.update();
+                    robot.followTrajectorySequenceAsync(Intake1);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    robot.followTrajectorySequenceAsync(Intake2);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
                     break;
                 }
-                robot.update();
-            }
-            robot.followTrajectorySequenceAsync(Intake2);
-            while (opModeIsActive() && !isStopRequested()){
-                if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
-                    robot.breakFollowing();
-                    robot.Intake.setPower(-1);
-                    robot.followTrajectorySequence(Blocks);
-                    robot.cargo.setPosition(0);
-                    sleep(1000);
-                    robot.cargo.setPosition(1);
+                robot.followTrajectorySequence(Intake2);
+
+                break;
+
+            case MIDDLE:
+                telemetry.addData("Middle","proceed");
+                telemetry.update();
+                robot.capper.setPosition(0);
+                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLUE);
+                robot.followTrajectorySequence(Middle);
+                robot.cargo.setPosition(0);
+                sleep(1000);
+                while (opModeIsActive() && !isStopRequested()) {
+                    telemetry.addData("Distance", robot.distance.getDistance(DistanceUnit.CM));
+                    telemetry.update();
+                    robot.followTrajectorySequenceAsync(Intake1);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    robot.followTrajectorySequenceAsync(Intake2);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
                     break;
                 }
-                robot.update();
-            }
-            break;
+                robot.followTrajectorySequence(Intake2);
+
+                break;
+
+            case NOT_FOUND:
+                telemetry.addData("Not Found","proceed");
+                telemetry.update();
+                robot.capper.setPosition(0);
+                robot.lights.setPattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+                robot.followTrajectorySequence(Right);
+                robot.cargo.setPosition(0);
+                sleep(1000);
+                while (opModeIsActive() && !isStopRequested()) {
+                    telemetry.addData("Distance", robot.distance.getDistance(DistanceUnit.CM));
+                    telemetry.update();
+                    robot.followTrajectorySequenceAsync(Intake1);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    robot.followTrajectorySequenceAsync(Intake2);
+                    while (opModeIsActive() && !isStopRequested()){
+                        if (robot.distance.getDistance(DistanceUnit.CM)< 7 && stopTimer.seconds() > 10){
+                            robot.breakFollowing();
+                            robot.Intake.setPower(-1);
+                            robot.followTrajectorySequence(Blocks);
+                            robot.cargo.setPosition(0);
+                            sleep(1000);
+                            robot.cargo.setPosition(1);
+                            break;
+                        }
+                        robot.update();
+                    }
+                    break;
+                }
+                robot.followTrajectorySequence(Intake2);
+
+                break;
+
+
         }
-        robot.followTrajectorySequence(Intake2);
 
 
         }
